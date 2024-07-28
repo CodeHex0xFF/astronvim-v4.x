a3cd396d9cb969e261f126136d0c3b35a899b7b7
hongdayu <hong_da_yu@163.com>
2024-07-28 18:52:41 +0800

    1.修改statusbar

--- lua/plugins/nvchad_statusline.lua
+++ lua/plugins/nvchad_statusline.lua
@@ -9,16 +9,16 @@ return {
         ScrollText = "",
         GitBranch = "",
         GitAdd = "",
-        GitChange = "",
+        GitChange = "󱀺",
         GitDelete = "",
       },
       -- modify variables used by heirline but not defined in the setup call directly
       status = {
         -- define the separators between each section
         separators = {
-          left = { "", "" }, -- separator for the left side of the statusline
+          left = { "", "" }, -- separator for the left side of the statusline
           right = { " ", "" }, -- separator for the right side of the statusline
-          tab = { "",  "" },
+          tab = { "",  "" },
         },
         -- add new colors that can be used by heirline
         colors = function(hl)
