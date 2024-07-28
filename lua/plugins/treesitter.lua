5bf5fcddb2966131add28d3b0b498eff6779c527
hongdayu <hong_da_yu@163.com>
2024-07-28 10:38:18 +0800

    1.4.x 初始化

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "c",
+      "cpp",
+      "python",
+      "rust",
+      "cmake",
+      "bash",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
